ed35be4396dfed0ae8c5c4549933110792db4a93
wixpor <jhonjhon921109@gmail.com>
2024-09-09 21:28:26 -0500

    Primer commit

---
+++ BotNotion2Sqlite/Program.cs
@@ -0,0 +1,44 @@
+﻿using SQLiteHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotNotion2Sqlite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbManager = new SQLiteDatabaseManager("C:\\BotGhl2Notion\\BatchNotion.db");
+
+
+            //dbManager.CreateDatabase();
+
+            //dbManager.CreateTable("NotionSync","NOSY_ID INTEGER PRIMARY KEY AUTOINCREMENT" +
+            //                                   ", NOSY_EMAIL TEXT NOT NULL UNIQUE" +
+            //                                   ", NOSY_INSERT_DATETIME DATETIME DEFAULT CURRENT_TIMESTAMP");
+
+
+
+            var Lista_NotionEmail = ControllerNotionSync.Get_Lst_Enti_NotionEmails();
+            if (Lista_NotionEmail._Status_Code == 200)
+            {
+                foreach(var notionEmail in Lista_NotionEmail.Lst_NotionSync)
+                {
+                    Console.WriteLine(notionEmail.NOSY_EMAIL);
+                }
+                Console.WriteLine(Lista_NotionEmail.Lst_NotionSync.Count);
+            }
+            else
+            {
+                Console.WriteLine(Lista_NotionEmail._Status_Code);
+                Console.WriteLine(Lista_NotionEmail._Error_Desc);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
